47ad705c3e7b50be0638c4c2cb58e2a0029c1708
Kevin Hernandez <k.hernandezt@uniandes.edu.co>
2024-09-06 14:12:01 -0500

    Arreglado para que los nombres de las variables y los macros puedan ser alfanumericos Comentados todos los prints

--- Lexer.py
+++ Lexer.py
@@ -5,7 +5,7 @@ orientations:list[str] = ["north", "south", "east", "west"]
 constants:list[str] = ["size", "myx", "myy", "mychips", "myballons", "balloonshere", "chipshere", "roomforchips"]
 commands:list[str] = ["walk", "jump", "drop", "pick", "grab", "letgo", "pop"]
 noChanges:list[str] = ["exec", "if", "fi", "else", "then", "do", "od", "rep", "times", "nop", "moves", "not",
-                        "isblocked?", "isfacing?", "zero?", "turntomy", "turntothe", "safeexe"]
+                        "isblocked?", "isfacing?", "zero?", "turntomy", "turntothe", "safeexe", "front"]
 symbols:list[str] = ["(", ")", "{", "}", ",", ";", ":","="]
 personalizedMacros:list[str] = []
 personalizedVariables:list[str] = []
@@ -79,50 +79,48 @@ def convertirATokens(listaPalabras: list[str],parametros_macros=[]) -> list[str]
     while(i < len(listaPalabras)):                       # Mientras no se haya recorrido toda la lista de palabras
         palabra:str = listaPalabras[i]                   # Se toma la palabra en la posicion i y se le asigna un token dependiendo de su tipo
         palabra=palabra.replace(" ","")
-        print("la palabra actual es:-"+palabra+"-")
-        print(parametros_macros)
+        #print("la palabra actual es:-"+palabra+"-")
+        #print(parametros_macros)
         if (palabra in noChanges) or (palabra in symbols): # Si la palabra es un token que no cambia se agrega a la lista de tokens tal cual
             listTokens.append(palabra)  
-            print("se agrega el token:",palabra)       
+            #print("se agrega el token:",palabra)       
         elif (palabra in constants) or (isNumber(palabra)) or (palabra in personalizedVariables) or (palabra in parametros_macros):                    
             listTokens.append("n")                       # Si la palabra es una constante, un numero o una variable se agrega un token n
-            print("se agrega el token:","n")  
+            #print("se agrega el token:","n")  
         elif (isNumber(palabra)):                        # Si la palabra es un numero
             listTokens.append("#")
         elif (palabra in commands):                      # Si la palabra es un comando
             listTokens.append("command")
-            print("se agrega el token:","command")  
+            #print("se agrega el token:","command")  
         elif (palabra in directions):                    # Si la palabra es una direccion
             listTokens.append("D")
-            print("se agrega el token:","D")  
+            #print("se agrega el token:","D")  
         elif (palabra in moveDirections):                # Si la palabra es una direccion de movimiento
             listTokens.append("D2")
-            print("se agrega el token:","D2")  
+            #print("se agrega el token:","D2")  
         elif (palabra in orientations):                  # Si la palabra es una orientacion
             listTokens.append("O")
-            print("se agrega el token:","O")  
+            #print("se agrega el token:","O")  
         elif (palabra in personalizedMacros):            # Si la palabra es un macro anteriormente definido
-           if listaPalabras[i-2] != "new" and i>0:
-                print("se agrega el token:","macro({})".format(palabra))  
-                listTokens.append("macro({})".format(palabra))
+           listTokens.append("macro({})".format(palabra))
 
         elif (palabra == "new"):                         # Si la palabra es NEW se debe ver si se esta definiendo una nueva variable o un nuevo macro
-            if (listaPalabras[i+1] == "var") and (listaPalabras[i+2].isalpha()):
+            listTokens.append("new")
+            if (listaPalabras[i+1] == "var") and (listaPalabras[i+2].isalpha() or listaPalabras[i+2].isalnum()):
                 personalizedVariables.append(listaPalabras[i+2])
                 i+=1
-            elif (listaPalabras[i+1] == "var") and not(listaPalabras[i+2].isalpha()):
+            elif (listaPalabras[i+1] == "var") and not(listaPalabras[i+2].isalpha() or listaPalabras[i+2].isalnum()):
                 nuevaVar:str = cleanPalabra(listaPalabras[i+2])[0] 
                 personalizedVariables.append(nuevaVar)
                 i+=1
-            elif (listaPalabras[i+1] == "macro") and (listaPalabras[i+2].isalpha()):
+            elif (listaPalabras[i+1] == "macro") and (listaPalabras[i+2].isalpha() or listaPalabras[i+2].isalnum()):
                 personalizedMacros.append(listaPalabras[i+2])
                 if listaPalabras[i+3] != "()":
                     listaParametros=procesarParametros(listaPalabras[i+3]+listaPalabras[i+4])
-                    listTokens.append("macro({})".format(listaPalabras[i+2]))
                     parametros_macros=parametros_macros+listaParametros
-                    print("yesid",listaParametros,parametros_macros)
+                    #print("yesid",listaParametros,parametros_macros)
                 i+=1 
-            elif (listaPalabras[i+1] == "macro") and not(listaPalabras[i+2].isalpha()):
+            elif (listaPalabras[i+1] == "macro") and not(listaPalabras[i+2].isalpha() or listaPalabras[i+2].isalnum()):
                 nuevaVar:str = cleanPalabra(listaPalabras[i+2])[0] 
                 personalizedVariables.append(nuevaVar)
                 i+=1
@@ -132,7 +130,7 @@ def convertirATokens(listaPalabras: list[str],parametros_macros=[]) -> list[str]
         else:                                                # Si en esa posicion no se encontro ninguno de los tokens anteriores
             listaPequeña:list[str] = cleanPalabra(palabra)     # Se limpia la palabra porque pueden ser simbolos mezclados con palabras o otros simbolos ej: "walk(1)"
             if listaPequeña != [palabra]:  
-                print("lista pequenia",listaPequeña)
+                #print("lista pequenia",listaPequeña)
                 tokens = convertirATokens(listaPequeña,parametros_macros)  # Se convierte la palabra limpia en tokens
                 listTokens.extend(tokens)                # Se agregan los tokens a la lista de tokens
                 #print(tokens, " - -- - ",palabra)
