67189b53633868467f51ac87827473b20e2f19d7
Kevin Hernandez <k.hernandezt@uniandes.edu.co>
2024-09-07 01:39:55 -0500

    Agregada la primera parte de validar condiciones

--- Parser.py
+++ Parser.py
@@ -7,3 +7,20 @@ def leerTokens(archivo: str) -> list[str]:
     return tokens
 
 tokens:list[str] = leerTokens("files/prueba")
+
+def validarCondiciones(tokens: list[str]) -> int:
+    i:int = 0
+    while i < len(tokens):
+        if (tokens[i] == "isblocked?"):
+            if (tokens[i+1] == "("):
+                if (tokens[i+2] in Lexer.directions) or tokens[i+2] == "front":
+                    if (tokens[i+3] == ")"):
+                        i+=4
+                        continue
+                    else:
+                        raise ValueError("Error de sintaxis: Falta un paréntesis de cierre.")
+                else:
+                    raise ValueError("Error de sintaxis: El parametro para la condicion isBlocked? debe ser una direccion (front, back, left, rigth) pero es {}.".format(tokens[i+2]))
+            else:
+                raise ValueError("Error de sintaxis: Falta un paréntesis de apertura.")
+        
