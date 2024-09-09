d91abe77dcffe9caa058e89e7916cafb2b47b832
Kevin Hernandez <k.hernandezt@uniandes.edu.co>
2024-09-08 19:01:03 -0500

    Eliminados todos los prints de pruebas y verificar funcionamiento. Commit final hopefully

--- Lexer.py
+++ Lexer.py
@@ -13,8 +13,6 @@ personalizedMacros:list[str] = []
 personalizedVariables:list[str] = []
 def revisar_corchetes(str_palabras: str,index:int) :
     str_palabras=" ".join(str_palabras[index:])
-    #print(index)
-    print(str_palabras)
     cerrado=False
     open_counter=0
     close_counter=0
@@ -35,11 +33,9 @@ def revisar_corchetes(str_palabras: str,index:int) :
         i+=1
     if cerrado==True:
         rta=str_palabras[indice_open:indice_close+1]
-        #print(rta) 
         return rta
     else:
         rta=str_palabras[indice_open:]
-        #print(rta) 
         return rta
 def extraer_parametros_turntomy(cadena: str):
     # Expresión regular para extraer todos los parámetros de turntomy
@@ -61,7 +57,6 @@ def extraer_parametros_turntothe(cadena: str):
         return "No se encontraron funciones turntothe."
 def procesarParametros(parametros: str,lista_palabras,index,flag=False) -> tuple[list[str], str]:
     if flag:
-        print("-"+parametros+"-")
         indice_parentesis = parametros.index("(")
         # Extrae desde el paréntesis de apertura hasta el final
         parametros = parametros[indice_parentesis:]
@@ -71,14 +66,10 @@ def procesarParametros(parametros: str,lista_palabras,index,flag=False) -> tuple
     parametros_o=[]
     if "turntomy" in macro_sin_espacios:
         parametros_d=extraer_parametros_turntomy(macro_sin_espacios)
-        #print(parametros_d)
         
     if "turntothe" in macro_sin_espacios:
         parametros_o=extraer_parametros_turntothe(macro_sin_espacios)
-        #print(parametros_o)
-        
-    #print(macro_sin_espacios)
-    #print(macro)
+
     # Verificar que los paréntesis están bien colocados
     # Verificar que los paréntesis están bien colocados
     if parametros.startswith("(") and parametros.endswith(")"):
@@ -118,9 +109,6 @@ def crearListaPalabras(archivo: str) -> list[str]:
     
     return listPalabras
 
-#print(crearListaPalabras("files/prueba"))
-
-
 def isNumber(palabra: str) -> bool:
     try:
         int(palabra)
@@ -151,35 +139,25 @@ def convertirATokens(listaPalabras: list[str],parametros_macros=[],param_O=[],pa
 
     while(i < len(listaPalabras)):                       # Mientras no se haya recorrido toda la lista de palabras
         palabra:str = listaPalabras[i]                   # Se toma la palabra en la posicion i y se le asigna un token dependiendo de su tipo
-        
-        #print("la palabra actual es:-"+palabra+"-")
-        #print(param_O)
+
         if (palabra in noChanges) or (palabra in symbols): # Si la palabra es un token que no cambia se agrega a la lista de tokens tal cual
             listTokens.append(palabra)  
-            #print("se agrega el token:",palabra)
         elif  (palabra in param_O):
             listTokens.append("O") 
-
         elif  (palabra in param_D):
             listTokens.append("D") 
-                   
         elif (palabra in constants) or (isNumber(palabra)) or (palabra in parametros_macros):                    
             listTokens.append("n")                       # Si la palabra es una constante o un numero se agrega un token n
-            #print("se agrega el token:","n")  
         elif (palabra in personalizedVariables):         # Si la palabra es un numero
             listTokens.append("variable")
         elif (palabra in commands):                      # Si la palabra es un comando
             listTokens.append("command")
-            #print("se agrega el token:","command")  
         elif (palabra in directions):                    # Si la palabra es una direccion
             listTokens.append("D")
-            #print("se agrega el token:","D")  
         elif (palabra in moveDirections):                # Si la palabra es una direccion de movimiento
             listTokens.append("D2")
-            #print("se agrega el token:","D2")  
         elif (palabra in orientations):                  # Si la palabra es una orientacion
             listTokens.append("O")
-            #print("se agrega el token:","O")  
         elif (palabra in personalizedMacros):            # Si la palabra es un macro anteriormente definido
            listTokens.append(palabra)
 
@@ -196,28 +174,20 @@ def convertirATokens(listaPalabras: list[str],parametros_macros=[],param_O=[],pa
                 personalizedMacros.append(listaPalabras[i+2])
                 listTokens.append("macro")
                 if listaPalabras[i+3] != "()":
-                    print("i",listaPalabras[i+3]+listaPalabras[i+4])
                     listaParametros,param_d,param_o=procesarParametros(listaPalabras[i+3]+listaPalabras[i+4],listaPalabras,i+4)
-                    print(listaPalabras[i+2])
-                    print(listaParametros)
                     parametros_macros=parametros_macros+listaParametros
                     param_D=param_D+param_d
                     param_O=param_O+param_o
-                    #print("yesid",listaParametros,parametros_macros)
                 i+=1 
             elif (listaPalabras[i+1] == "macro") and not(listaPalabras[i+2].isalpha() or listaPalabras[i+2].isalnum()):
                 nuevoMacro:str = cleanPalabra(listaPalabras[i+2])[0] 
                 listTokens.append("macro")
                 personalizedMacros.append(nuevoMacro)
-                print("nueva +i+macro",nuevoMacro)
-                print(listaPalabras[i+2])
                 if listaPalabras[i+3] != "()":
                     listaParametros,param_d,param_o=procesarParametros(listaPalabras[i+2],listaPalabras,i,True)
-                    print(listaParametros)
                     parametros_macros=parametros_macros+listaParametros
                     param_D=param_D+param_d
                     param_O=param_O+param_o
-                    #print("yesid",listaParametros,parametros_macros)
                 i+=1
 
         elif(palabra == ""):
@@ -225,13 +195,11 @@ def convertirATokens(listaPalabras: list[str],parametros_macros=[],param_O=[],pa
         else:                                                # Si en esa posicion no se encontro ninguno de los tokens anteriores
             listaPequeña:list[str] = cleanPalabra(palabra)     # Se limpia la palabra porque pueden ser simbolos mezclados con palabras o otros simbolos ej: "walk(1)"
             if listaPequeña != [palabra]:  
-                #print("lista pequenia",listaPequeña)
                 tokens = convertirATokens(listaPequeña,parametros_macros,param_O,param_D)  # Se convierte la palabra limpia en tokens
                 listTokens.extend(tokens)                # Se agregan los tokens a la lista de tokens
-                #print(tokens, " - -- - ",palabra)
             else:
                 listTokens.append("ERROR")  # Si no se encontro ningun token se agrega un error a la lista        
-                print("Se agregó un error en la palabra:",palabra)  
+                print("Se agregó un error en la palabra:",palabra)
         i+=1                                             # Se aumenta el contador para pasar a la siguiente palabra
     
     return listTokens
@@ -264,5 +232,3 @@ def imprimirTokensNumerados(tokens:list[str]) -> None:
     while i < len(tokens):
         print(tokens[i]+" ---- #{}".format(i))
         i+=1
-
-#print(" ".join(tokens))
